a5555f6a143f6e18246a0cc6ca3ed1fc7e45ede5
aadithp <ci19639@bristol.ac.uk>
2022-05-06 04:16:12 +0100

    Added sparsity hyperparameter

--- agent/icm.py
+++ agent/icm.py
@@ -5,6 +5,39 @@ import utils
 from agent.sac import SACAgent
 
 
+def weights_init_(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight, gain=1)
+        nn.init.constant_(m.bias, 0)
+
+
+class Linear_Encoder(nn.Module):
+    def __init__(
+        self, obs_dim, hidden_dim, output_dim, activation=nn.LeakyReLU(inplace=True)
+    ):
+        super().__init__()
+        init_weights = 3e-3
+        modules_state = []
+        modules_state.append(nn.Linear(obs_dim, hidden_dim))
+        modules_state.append(activation)
+
+        # for i in range(hidden_layers - 1):
+        # 	modules_state.append(nn.Linear(hidden_dim, hidden_dim))
+        # 	modules_state.append(activation)
+
+        last_fc = nn.Linear(hidden_dim, output_dim)
+        last_fc.weight.data.uniform_(-init_weights, init_weights)
+        last_fc.bias.data.uniform_(-init_weights, init_weights)
+        modules_state.append(last_fc)
+
+        self.fc = nn.Sequential(*modules_state)
+        self.apply(weights_init_)
+
+    def forward(self, inputs):
+        outputs = self.fc(inputs)
+        return outputs
+
+
 class ICM(nn.Module):
     def __init__(self, obs_dim, action_dim, hidden_dim):
         super().__init__()
@@ -39,17 +72,34 @@ class ICM(nn.Module):
 
 
 class ICMAgent(SACAgent):
-    def __init__(self, icm_scale, reward_free, **kwargs):
+    def __init__(self, icm_scale, sparsity, reward_free, **kwargs):
         super().__init__(**kwargs)
         self.icm_scale = icm_scale
         self.reward_free = reward_free
+        self.sparsity = sparsity
+
+        if self.enc_type == "linear":
+            self.l4_encoder = Linear_Encoder(
+                self.obs_dim, self.hidden_dim, self.repr_dim
+            ).to(self.device)
+            self.obs_dim = self.repr_dim
+        else:
+            self.l4_encoder = nn.Identity()
 
         self.icm = ICM(self.obs_dim, self.action_dim, self.hidden_dim).to(self.device)
 
         # optimizers
+        if self.enc_type == "linear":
+            self.l4_encoder_opt = torch.optim.Adam(
+                self.l4_encoder.parameters(), lr=self.lr
+            )
+        else:
+            self.l4_encoder_opt = None
+
         self.icm_optimizer = torch.optim.Adam(self.icm.parameters(), lr=self.lr)
 
         self.icm.train()
+        self.l4_encoder.train()
 
     def update_icm(self, obs, action, next_obs, step):
         metrics = dict()
@@ -58,9 +108,15 @@ class ICMAgent(SACAgent):
 
         loss = forward_error.mean() + backward_error.mean()
 
+        if self.l4_encoder_opt is not None:
+            self.l4_encoder_opt.zero_grad(set_to_none=True)
+
         self.icm_optimizer.zero_grad(set_to_none=True)
         loss.backward()
         self.icm_optimizer.step()
+
+        if self.l4_encoder_opt is not None:
+            self.l4_encoder_opt.step()
 
         if self.use_tb or self.use_wandb:
             metrics["icm_loss"] = loss.item()
@@ -76,6 +132,9 @@ class ICMAgent(SACAgent):
         reward = torch.log(reward + 1.0)
         return reward
 
+    def encode(self, obs):
+        return self.l4_encoder(obs)
+
     def update(self, replay_iter, step):
         metrics = dict()
 
@@ -87,15 +146,19 @@ class ICMAgent(SACAgent):
             batch, self.device
         )
 
-        metrics.update(self.update_icm(obs, action, next_obs, step))
+        state = self.encode(obs)
+        with torch.no_grad():
+            next_state = self.encode(next_obs)
+
+        metrics.update(self.update_icm(state, action, next_state, step))
 
         with torch.no_grad():
-            intr_reward = self.compute_intr_reward(obs, action, next_obs, step)
+            intr_reward = self.compute_intr_reward(state, action, next_state, step)
 
         if self.use_tb or self.use_wandb:
             metrics["intr_reward"] = intr_reward.mean().item()
 
-        if self.reward_free:
+        if step % self.sparsity != 0:
             reward = intr_reward
         else:
             reward = extr_reward + intr_reward
@@ -106,11 +169,11 @@ class ICMAgent(SACAgent):
 
         # update critic
         metrics.update(
-            self.update_critic(obs, action, reward, discount, next_obs, step)
+            self.update_critic(state.detach(), action, reward, discount, next_state.detach(), step)
         )
 
         # update actor
-        metrics.update(self.update_actor_and_alpha(obs, step))
+        metrics.update(self.update_actor_and_alpha(state.detach(), step))
 
         # update critic target
         utils.soft_update_params(
